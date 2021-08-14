832ab9910f89bac49d632f9ab0f17006d71737c6
Oli Ullah Jewel <oliullahjewel7@gmail.com>
2021-08-14 20:47:25 +0600

    Finished updating the UI

--- app.js
+++ app.js
@@ -61,6 +61,23 @@ const ItemCtrl = (function() {
 
             return found;
         },
+        updatedItem: function(name, calories) {
+            //calories to number
+
+            calories = parseInt(calories);
+
+            let found = null;
+
+            data.items.forEach(function(item) {
+                if (item.id === data.currentItem.id) {
+                    item.name = name;
+                    item.calories = calories;
+                    found = item;
+                }
+            });
+
+            return found;
+        },
         setCurrentItem: function(item) {
             data.currentItem = item;
         },
@@ -91,6 +108,7 @@ const ItemCtrl = (function() {
 const UICtrl = (function() {
     const UISelectors = {
         itemList: "#item-list",
+        listItems: "#item-list li",
         addBtn: ".add-btn",
         updateBtn: ".update-btn",
         deleteBtn: ".delete-btn",
@@ -151,6 +169,25 @@ const UICtrl = (function() {
                 .querySelector(UISelectors.itemList)
                 .insertAdjacentElement("beforeend", li);
         },
+        updateListItem: function(item) {
+            let listItems = document.querySelectorAll(UISelectors.listItems);
+
+            // Turn node into list array
+            listItems = Array.from(listItems);
+
+            listItems.forEach(function(listItem) {
+                const itemID = listItem.getAttribute("id");
+
+                if (itemID === `item-${item.id}`) {
+                    document.querySelector(
+                        `#${itemID}`
+                    ).innerHTML = `<strong>${item.name}: </strong> <em>${item.calories} Calories</em>
+                <a href="#" class="secondary-content">
+                    <i class="edit-item fa fa-pencil"></i>
+                </a>`;
+                }
+            });
+        },
         clearInput: function() {
             document.querySelector(UISelectors.itemNameInput).value = "";
             document.querySelector(UISelectors.itemCalorieInput).value = "";
@@ -201,10 +238,23 @@ const App = (function(ItemCtrl, UICtrl) {
             .querySelector(UISelectors.addBtn)
             .addEventListener("click", itemAddSubmit);
 
+        //Disable submit on enter
+
+        document.addEventListener("keypress", function(e) {
+            if (e.keyCode === 13 || e.which === 13) {
+                e.preventDefault();
+                return false;
+            }
+        });
         // Edit item event
 
         document
             .querySelector(UISelectors.itemList)
+            .addEventListener("click", itemEditClick);
+        // Update item event
+
+        document
+            .querySelector(UISelectors.updateBtn)
             .addEventListener("click", itemUpdateSubmit);
     };
 
@@ -237,9 +287,9 @@ const App = (function(ItemCtrl, UICtrl) {
         e.preventDefault();
     };
 
-    //Update item submit
+    //Item edit click
 
-    const itemUpdateSubmit = function(e) {
+    const itemEditClick = function(e) {
         if (e.target.classList.contains("edit-item")) {
             //Get list item (item-0, item-1)
             const listId = e.target.parentNode.parentNode.id;
@@ -263,6 +313,32 @@ const App = (function(ItemCtrl, UICtrl) {
             UICtrl.addItemToForm();
         }
 
+        e.preventDefault();
+    };
+
+    // Update item submit
+
+    const itemUpdateSubmit = function(e) {
+        //Get item input
+
+        const input = UICtrl.getItemInput();
+
+        //Update item
+
+        const updatedItem = ItemCtrl.updatedItem(input.name, input.calories);
+
+        //Update the UI
+        UICtrl.updateListItem(updatedItem);
+
+        //Get total calories
+
+        const totalCalories = ItemCtrl.getTotalCalories();
+
+        //Show total calories to UI
+
+        UICtrl.showTotalCalories(totalCalories);
+
+        UICtrl.clearEditState();
         e.preventDefault();
     };
     //Public Methods
